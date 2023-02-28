82555a53196967833257548ae36d5774623a2703
i-norden <iansnorden@gmail.com>
2023-02-28 11:34:21 -0600

    update internal database interface

--- database.go
+++ database.go
@@ -3,11 +3,10 @@ package ipld_eth_statedb
 import (
 	"context"
 	"errors"
-	"fmt"
 
 	"github.com/VictoriaMetrics/fastcache"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/statediff/indexer/ipld"
 	"github.com/ethereum/go-ethereum/trie"
@@ -28,26 +27,28 @@ var (
 	errNotFound = errors.New("not found")
 )
 
-
+// Database interface is a union of the subset of the geth state.Database interface required
+// to support the vm.StateDB implementation as well as methods specific to this Postgres based implementation
 type Database interface {
 	ContractCode(addrHash common.Hash, codeHash common.Hash) ([]byte, error)
 	ContractCodeSize(addrHash common.Hash, codeHash common.Hash) (int, error)
-	OpenTrie(root common.Hash) (state.Trie, error)
-	OpenStorageTrie(addrHash common.Hash, root common.Hash) (state.Trie, error)
-	CopyTrie(trie state.Trie) state.Trie
+	StateAccount(address common.Address) (*types.StateAccount, error)
+	StorageSlot(addressHash, slotHash common.Hash) ([]byte, error)
 }
 
+var _ Database = &stateDatabase{}
+
 type stateDatabase struct {
-	pgdb     pgxpool.Pool
-	trieDB *trie.Database
+	pgdb          *pgxpool.Pool
+	trieDB        *trie.Database
 	codeSizeCache *lru.Cache
 	codeCache     *fastcache.Cache
 }
 
-func NewStateDatabase(pgdb pgxpool.Pool, ethdb ethdb.Database, config *trie.Config) (*stateDatabase, error) {
+func NewStateDatabase(pgdb *pgxpool.Pool, ethdb ethdb.Database, config *trie.Config) (*stateDatabase, error) {
 	csc, _ := lru.New(codeSizeCacheSize)
 	return &stateDatabase{
-		pgdb: 		   pgdb,
+		pgdb:          pgdb,
 		trieDB:        trie.NewDatabaseWithConfig(ethdb, config),
 		codeSizeCache: csc,
 		codeCache:     fastcache.New(codeCacheSize),
@@ -79,27 +80,10 @@ func (sd *stateDatabase) ContractCodeSize(_, codeHash common.Hash) (int, error) 
 	return len(code), err
 }
 
-func (sd *stateDatabase) OpenTrie(root common.Hash) (state.Trie, error) {
-	tr, err := trie.NewStateTrie(common.Hash{}, root, sd.trieDB)
-	if err != nil {
-		return nil, err
-	}
-	return tr, nil
+func (sd *stateDatabase) StateAccount(address common.Address) (*types.StateAccount, error) {
+	panic("implement me")
 }
 
-func (sd *stateDatabase) OpenStorageTrie(addrHash common.Hash, root common.Hash) (state.Trie, error) {
-	tr, err := trie.NewStateTrie(addrHash, root, sd.trieDB)
-	if err != nil {
-		return nil, err
-	}
-	return tr, nil
-}
-
-func (sd *stateDatabase) CopyTrie(t state.Trie) state.Trie {
-	switch t := t.(type) {
-	case *trie.StateTrie:
-		return t.Copy()
-	default:
-		panic(fmt.Errorf("unknown trie type %T", t))
-	}
+func (sd *stateDatabase) StorageSlot(addressHash, slotHash common.Hash) ([]byte, error) {
+	panic("implement me")
 }
